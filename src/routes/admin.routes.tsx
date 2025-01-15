96fb9d181ed195fa78b5afac8c1ddf26918fb4ca
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-15 16:29:11 +0600

    sdgf

---
+++ src/routes/admin.routes.tsx
@@ -0,0 +1,9 @@
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import CreateStudent from "../pages/admin/CreateStudent";
+
+export const adminPaths = [
+  { path: "create-student", element: <CreateStudent /> },
+  { path: "create-admin", element: <CreateAdmin /> },
+  { path: "create-faculty", element: <CreateFaculty /> },
+];
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,10 +3,8 @@ import App from "../App";
 import Contact from "../pages/contact";
 import About from "../pages/about";
 import Login from "../pages/login";
-import Register from "../pages/register";
-import CreateStudent from "../pages/admin/CreateStudent";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty";
+import Register from "../pages/register"; 
+import { adminPaths } from "./admin.routes";
 
 export const router = createBrowserRouter([
   {
@@ -20,11 +18,17 @@ export const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: [
-      { path: "create-student", element: <CreateStudent /> },
-      { path: "create-admin", element: <CreateAdmin /> },
-      { path: "create-faculty", element: <CreateFaculty /> },
-    ],
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminPaths,
   },
   { path: "login", element: <Login /> },
   { path: "register", element: <Register /> },
