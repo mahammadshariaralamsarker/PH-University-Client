b6d28886eb1cf3f69819b4e45f8bee1fbbe29ad9
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-19 21:26:57 +0600

    asdfa

--- src/components/layout/MainLayOut.tsx
+++ src/components/layout/MainLayOut.tsx
@@ -1,67 +1,17 @@
-import { Layout, Menu  } from "antd";
+import { Layout    } from "antd";
 import {   Outlet } from "react-router-dom";
-import { adminPaths } from "../../routes/admin.routes";
-import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
-const { Header, Content, Footer, Sider } = Layout;
-// const items: MenuProps["items"] = [
-//   {
-//     key: "Dashboard",
-//     label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-//   },
-//   {
-//     key: "User Management",
-//     label: "User Management",
-//     children: [
-//       {
-//         key: 11,
-//         label: <NavLink to="/admin/create-admin"> Create Admin</NavLink>,
-//       },
-//       {
-//         key: 12,
-//         label: <NavLink to="/admin/create-faculty"> Create Faculty</NavLink>,
-//       },
-//       {
-//         key: 13,
-//         label: <NavLink to="/admin/create-student"> Create Student</NavLink>,
-//       },
-//     ],
-//   },
-// ];
+ 
+import Sidebar from "./sidebar";
+const { Header, Content, Footer } = Layout;
+ 
 export default function MainLayOut() {
   return (
     <div>
       {" "}
       <Layout style={{ height: "100vh" }}>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="demo-logo-vertical" />
-          <div
-            style={{
-              color: "white",
-              textAlign: "center",
-              height: "3rem",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <h1>PH</h1>
-          </div>
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={["4"]}
-            items={sidebarItemsGenerator(adminPaths, 'admin')}
-          />
-        </Sider>
+      <div>
+        <Sidebar/>
+      </div>
         <Layout>
           <Header style={{ padding: 0 }} />
           <Content style={{ margin: "24px 16px 0" }}>
---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,43 @@
+import { Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import React from "react";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
+
+export default function Sidebar() {
+  return (
+    <div>
+      {" "}
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <div
+          style={{
+            color: "white",
+            textAlign: "center",
+            height: "3rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={sidebarItemsGenerator(adminPaths, "admin")}
+        />
+      </Sider>
+    </div>
+  );
+}
