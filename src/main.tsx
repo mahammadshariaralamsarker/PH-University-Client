916a4d2bec762f287ae3edfde58aec66109e282d
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-19 09:30:49 +0600

    fas

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { router } from "./routes/routes.tsx";
 import { RouterProvider } from "react-router-dom";
 import { store } from "./redux/store.ts";
-
+import { Provider } from "react";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
