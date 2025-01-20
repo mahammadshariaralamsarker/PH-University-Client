99a6762a22d85abbc0645c162df73a3b030b61bc
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-20 21:28:22 +0600

    asdf

--- src/redux/feature/auth/authApi.ts
+++ src/redux/feature/auth/authApi.ts
@@ -1,13 +1,14 @@
 import { baseApi } from "../../api/baseApi";
 
 const authApi = baseApi.injectEndpoints({
-  endpoints:(builder) =>({
+  endpoints: (builder) => ({
     login: builder.mutation({
-      query:(userInfo)=>({
-        url: '/auth/login',
-        method:'POST',
-        body:userInfo
-      })
-    })
-  })
-})
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+});
+export const {useLoginMutation} = authApi
