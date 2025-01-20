1928fa07f91fb37ba32be39ef5190a2005a27f07
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-20 22:09:20 +0600

    asdf

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>login page</div>
-  )
+    <form>
+      
+
+      <div className="flex ">
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" />
+      </div>
+    </form>
+  );
 }
